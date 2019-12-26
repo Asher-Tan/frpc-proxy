22808854f01edcee887cd94dbed143419fd3b445
ashertam <ashertam@gmail.com>
2019-12-26 11:21:36 +0800

    fix sqlite config

--- config/default.js
+++ config/default.js
@@ -4,6 +4,6 @@ module.exports = {
   target: 'http://192.168.1.110:3000',
   sequelize: {
     dialect: 'sqlite',
-    storage: path.join(__dirname, 'frpc-proxy.sqlite'),
+    storage: path.join(__dirname, '../data/frpc-proxy.sqlite'),
   },
 };
